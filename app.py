c76629ec4232c3ac53eeec02d8a38590c951f3dd
Sherman Hui <sherman.sy.hui@gmail.com>
2021-01-02 13:25:39 -0800

    feat: create simple demo day order Slack app
    Created a scheduled AWS lambda function is scheduled
    to run weekly every Thursday at 8am starting on 2021-01-07.
    The function will check if the given Thursday is a
    valid demo day. If it is, it will post a message and if not it will do
    nothing.
    
    There is also a simple command `/slash-get-list` endpoint that
    is hooked up to the DemoDayOrder Slack app command `/get-demo-order`
    so users can run `/get-demo-order` on the channel the app is added
    to generate a list on demand. The endpoint is prefixed with 'slash' to
    be explicit that this route is to be used as a slash command on Slack.
    
    There are also endpoints scaffolded out to set up the app for interactive
    Slack commands that I did not get to during the winter break. Ideally
    we should be able to set a custom start time, verify scheduled dates
    and delete a schedule. This will likely require setting up
    a dynamoDB instance to save the start date and scheduled dates to verify against
    instead of using the hard coded list of dates in the util function
    `verify_is_valid_demo_day`

---
+++ app.py
@@ -0,0 +1,81 @@
+import os
+import requests
+from flask import Flask
+from flask import jsonify
+from flask import request
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+from utils import create_markdown_list
+from utils import generate_randomized_list
+from utils import generate_demo_order_blocks
+from utils import verify
+from utils import verify_is_valid_demo_day
+
+# channel_id = os.environ["SLACK_CHANNEL_ID"]
+slack_token = os.environ["SLACK_OAUTH_TOKEN"]
+sc = WebClient(token=slack_token)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Homepage"
+
+
+@app.route("/set-start-date", methods=["POST"])
+def set_start_time():
+    # if no start time found
+    # prompt for start date
+    # calculate all valid demo days starting from 14 days from start date
+    # store list of valid demo days in db
+    # else if there is a start time
+    # confirm overwrite
+    pass
+
+
+@app.route("/get-scheduled-dates", methods=["GET", "POST"])
+def get_scheduled_dates():
+    # if there is a stored list of dates
+    # get current date
+    # return list of valid demo days after the current date
+    # else if no dates stored
+    # return no scheduled dates msg
+    pass
+
+
+@app.route("/delete-schedule", methods=["POST"])
+def delete_schedule():
+    # if there are valid demo days
+    # delete stored valid demo days
+    # else if no dates stored then no action is taken
+    # alert nothing was done
+    pass
+
+
+@app.route("/slash-get-list", methods=["POST"])
+def slash_get_list():
+    if verify(request):
+        return {
+            "response_type": "in_channel",
+            "blocks": generate_demo_order_blocks(),
+        }
+    else:
+        return {
+            "response_type": "in_channel",
+            "text": "An error occured with this request"
+        }
+
+
+def post_scheduled_demo_order_msg(event, context):
+    if verify_is_valid_demo_day():
+        sc.chat_postMessage(
+            channel="C01GK7J30A3",  # update to accept a custom channel id
+            blocks=generate_demo_order_blocks(
+                    "It's Demo Day! Here are the order of presentations:"
+                ),
+        )
+
+    # silently fail and do nothing if it is not a demo day
+    return
